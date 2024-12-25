fb285eca1d8e29e13528a87cb7a832ad554106be
atulRaaj <atulraj1010@tutanota.com>
2024-12-26 00:18:25 +0530

    location after error dont send

--- src/components/MyLocation.tsx
+++ src/components/MyLocation.tsx
@@ -73,11 +73,6 @@ export default function MyLocation({ onLocationUpdate }: MyLocationProps) {
     );
   };
 
-  useEffect(() => {
-    console.log("Location data updated:", locationData);
-    if (locationData)
-      onLocationUpdate(locationData.longitude, locationData.latitude);
-  }, [locationData]);
 
   const stopTracking = () => {
     if (watchId.current !== null) {
@@ -97,6 +92,8 @@ export default function MyLocation({ onLocationUpdate }: MyLocationProps) {
   const handleToggle = () => {
     if (!isTracking) {
       startTracking();
+      if (locationData)
+        onLocationUpdate(locationData.longitude, locationData.latitude);
     } else {
       stopTracking();
     }
